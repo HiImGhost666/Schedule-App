41a52b1e813de4474af680cb15c8344997da0809
Rodri <rriffo.99@gmail.com>
2026-04-21 14:10:07 +0100

    refactorizacion de esquema, eliminacion de variable calendarType y uniendo migraciones

--- backend/prisma/migrations/20260417184500_add_user_branch_assignment/migration.sql
+++
@@ -1,7 +0,0 @@
-PRAGMA foreign_keys=OFF;
-
-ALTER TABLE "users" ADD COLUMN "branch_id" TEXT REFERENCES "branches" ("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
-CREATE INDEX IF NOT EXISTS "users_branch_id_idx" ON "users"("branch_id");
-
-PRAGMA foreign_keys=ON;
